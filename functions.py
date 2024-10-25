faf053e09d5f0f8debc3d1fb1bb6e9cf113fb69f
SantiagoMag <santiagogem29@gmail.com>
2024-10-24 19:09:40 -0500

    UPD functions

--- functions.py
+++ functions.py
@@ -1,10 +1,24 @@
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import joblib
 
-
+def transform_loan_grade(data):
+    return data.replace(['A', 'B', 'C', 'D', 'E', 'F', 'G'], [1, 2, 3, 4, 5, 6, 7])
 
 def preprocessing(df):
-    df_ = df[['person_income','loan_amnt', 'person_age', 'loan_int_rate']]
+
     scaler = joblib.load('scaler.pkl')
+    lb_encoder_home_ownership = joblib.load('lb_encoder_home_ownership.pkl')
+    lb_encoder_loan_intent = joblib.load('lb_encoder_loan_intent.pkl')
+    lb_encoder_cb_person_default_on_file = joblib.load('lb_encoder_cb_person_default_on_file.pkl')
+    
+    df["home_ownership"] = lb_encoder_home_ownership.transform(df["home_ownership"])  
+    df["loan_intent"] = lb_encoder_loan_intent.transform(df["loan_intent"])  
+    df["cb_person_default_on_file"] = lb_encoder_cb_person_default_on_file.transform(df["cb_person_default_on_file"])  
+    df["loan_grade"] = transform_loan_grade(df["loan_grade"])  
+
+    df_ = df[
+        ['person_income', 'cb_person_default_on_file', 'person_home_ownership', 'loan_int_rate', 'loan_grade', 'loan_percent_income']
+        ]
     df_ = scaler.transform(df_)
+    
     return df_
